ff9dc8051d0091b84f2a0e836149e16989562786
Richard Meissner <rmeissner@popstart.intern>
2019-06-05 13:42:22 +0200

    Added uniswap

---
+++ uni/precache-manifest.abd3740de3b38663f2730daa8a618561.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a969660740ee483b398a3d28b16c35d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7d1966b88c4f2859e19",
+    "url": "./static/css/0.07de6c03.chunk.css"
+  },
+  {
+    "revision": "d236314e031d37b0cdfe",
+    "url": "./static/css/3.d75e0ccb.chunk.css"
+  },
+  {
+    "revision": "c7d1966b88c4f2859e19",
+    "url": "./static/js/0.1964e5f0.chunk.js"
+  },
+  {
+    "revision": "d236314e031d37b0cdfe",
+    "url": "./static/js/3.c617d113.chunk.js"
+  },
+  {
+    "revision": "b0160b420d2b21f5d1b3",
+    "url": "./static/js/4.80e7158e.chunk.js"
+  },
+  {
+    "revision": "17ee525aabcce1c0969d",
+    "url": "./static/js/5.cfaa31c4.chunk.js"
+  },
+  {
+    "revision": "7e26ba0ce50c5fd5e271",
+    "url": "./static/js/6.8c18e755.chunk.js"
+  },
+  {
+    "revision": "c02578cb44adead1e754",
+    "url": "./static/js/7.c04d2423.chunk.js"
+  },
+  {
+    "revision": "598ccfdd96f5f3b7643f",
+    "url": "./static/js/8.5596de40.chunk.js"
+  },
+  {
+    "revision": "a7b9b664ae49a77d815a",
+    "url": "./static/js/9.b311a288.chunk.js"
+  },
+  {
+    "revision": "0cf42d9d41e1671274d3",
+    "url": "./static/js/main.a8b8eb7a.chunk.js"
+  },
+  {
+    "revision": "4d72ac7e510c64178946",
+    "url": "./static/js/runtime~main.ecb2decc.js"
+  },
+  {
+    "revision": "cd061363bbf9cd7a26cb09b642dcaf63",
+    "url": "./static/media/arrow-down-blue.cd061363.svg"
+  },
+  {
+    "revision": "c0dedd2f2ed0c4d07d7ca75af3f0a65f",
+    "url": "./static/media/arrow-down-grey.c0dedd2f.svg"
+  },
+  {
+    "revision": "2d975615c4c409c3b6b00e8ae7c5767a",
+    "url": "./static/media/circle.2d975615.svg"
+  },
+  {
+    "revision": "b20914ec5482543a0b1b2c6d5509ab96",
+    "url": "./static/media/dropdown-blue.b20914ec.svg"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "./static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "b96d70e10dd30a64a0d122603577c8ae",
+    "url": "./static/media/dropup-blue.b96d70e1.svg"
+  },
+  {
+    "revision": "802c6eacbb13c83b27c39be505228231",
+    "url": "./static/media/ethereum-logo.802c6eac.svg"
+  },
+  {
+    "revision": "674400972753804891ec372652944539",
+    "url": "./static/media/magnifying-glass.67440097.svg"
+  },
+  {
+    "revision": "e8021e51723d455b2f9fa2446808d2a3",
+    "url": "./static/media/plus-blue.e8021e51.svg"
+  },
+  {
+    "revision": "d8e0be7d6efb0b53c37eb75e44b35bda",
+    "url": "./static/media/plus-grey.d8e0be7d.svg"
+  }
+]);
