115a2662a25f6c2ed819e8f06294b375536f4f15
Richard Meissner <richard@gnosis.io>
2019-06-21 14:04:37 +0200

    Update page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c5fdd95bb342e737e5eac066af34fbe4.js"
+  "./precache-manifest.426abf0245a688382f621fe1774803c3.js"
 );
 
 self.addEventListener('message', (event) => {
