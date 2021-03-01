eb60bc332690bf57186c77f12ac9011c3619d7bf
Richard Meissner <msc.meissner@gmail.com>
2021-03-01 21:59:44 +0100

    Clean website

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://github.com/rmeissner"
+          rel="noopener noreferrer"
+        >
+          <img src="./images/profile.jpg" className="App-logo" alt="logo" />
+          <p>
+            Richard Meissner
+          </p>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
